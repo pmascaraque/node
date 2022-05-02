2c0a0ca7f1064f59b1877d29c1d68bab0245d0ab
Pablo Moreno <ssm.pablo@gmail.com>
2022-05-03 01:31:33 +0200

    filtered ls

--- workshop/filtered-ls.js
+++ workshop/filtered-ls.js
@@ -1,1 +1,17 @@
-console.log("test");
+const fs = require('fs');
+
+const dirname = process.argv[2];
+const ext = '.' + process.argv[3];
+
+fs.readdir(dirname, (err, list) => { //read directory
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  list.forEach(file => {
+    if (file.endsWith(ext)) {
+      console.log(file);
+    }
+  });
+});
