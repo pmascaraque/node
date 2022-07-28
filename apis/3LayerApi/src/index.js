1a7c78965ce9f989f68a95867f71b0a932b8c174
Pablo Moreno <ssm.pablo@gmail.com>
2022-07-28 20:25:59 +0200

    versions

--- apis/3LayerApi/src/index.js
+++ apis/3LayerApi/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const v1Router = require("./v1/routes/index");
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.use("/api/v1", v1Router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
