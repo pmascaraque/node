30942eb45b81a519b5d28318faa9287f9f8cf9d0
Pablo Moreno <ssm.pablo@gmail.com>
2022-07-28 22:11:50 +0200

    dummy ORM and DB

--- apis/3LayerApi/src/controllers/workoutController.js
+++ apis/3LayerApi/src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require("../services/workoutService");
+
 const getAllWorkouts = async (req, res) => {
+  const allWorkouts = workoutService.getAllWorkouts();
   res.send("Get all workouts");
 }
 
 const getOneWorkout = async (req, res) => {
+  const workout = workoutService.getOneWorkout(req.params.workoutId);
   res.send(`Get workout ${req.params.workoutId}`);
 }
 
 const createOneWorkout = async (req, res) => {
+  const createdWorkout = workoutService.createOneWorkout(req.body);
   res.send(`Create workout ${req.params.workoutId}`);
 }
 
 const updateOneWorkout = async (req, res) => {
+  const updatedWorkout = workoutService.updateOneWorkout(req.params.workoutId);
   res.send(`Update workout ${req.params.workoutId}`);
 }
 
 const deleteOneWorkout = async (req, res) => {
+  workoutService.deleteOneWorkout(req.params.workoutId);
   res.send(`Delete workout ${req.params.workoutId}`);
 }
 
---
+++ apis/3LayerApi/src/services/workoutService.js
@@ -0,0 +1,15 @@
+const getAllWorkouts = () => {
+  return;
+}
+const getOneWorkout = () => {
+  return;
+}
+const createOneWorkout = () => {
+  return;
+}
+const updateOneWorkout = () => {
+  return;
+}
+const deleteOneWorkout = () => {
+  return;
+}
