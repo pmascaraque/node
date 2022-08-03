c7f3850183f45a547abf9d0013579edd123cd393
Pablo Moreno <ssm.pablo@gmail.com>
2022-08-04 01:06:32 +0200

    express server

---
+++ ts_workshop/src/@types/module.d.ts
@@ -0,0 +1,4 @@
+declare module 'module' {
+  import { Module } from 'module';
+  export = Module;
+}
--- ts_workshop/src/index.ts
+++ ts_workshop/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello2 from typescript")
+import express from 'express';
+
+const app = express();
+
+const add = (a: number, b?: number): number => {
+  if (b) {
+    return a + b;
+  } else {
+    return a;
+  }
+}
+
+app.get('/', (req) => {
+  add(1);
+});
+
+app.listen(3001, () => {
+  console.log('Started');
+});
